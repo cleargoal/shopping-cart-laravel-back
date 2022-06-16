6ad3a6eee4e915ff3af501905f82d607191f864b
best <cleargoal01@gmail.com>
2022-06-16 19:57:22 +0300

    add discounts seeder

--- database/seeders/DiscountSeeder.php
+++ database/seeders/DiscountSeeder.php
@@ -17,9 +17,9 @@ class DiscountSeeder extends Seeder
     {
         $default = [
             ['title' => 'At least 2 prods in diff cats exclude seeds', 'requirements' =>
-                '{"requirements":[{"target":{"categories":[-1]},"items":2}],"include":[],"exclude":[{"categories":[4],"exactly":[76,82]}],"value":{"type":"perc","amount":5}}'],
+                '{"requirements":[{"target":{"categories":[-1]},"products":2}],"include":[],"exclude":[{"categories":[4],"exactly":[76,82]}],"value":{"type":"perc","amount":5}}'],
             ['title' => 'At least 3 prods in cat A & 1 prod in C ', 'requirements' =>
-            '{"requirements":[{"target":{"categories":[3]},"items":1}],"include":[{"target":{"categories":[1]},"items":3}],"exclude":[{"categories":[4],"exactly":[76,82]}],"value":{"type":"perc","amount":20}}',]
+            '{"requirements":[{"target":{"categories":[3]},"products":1}],"include":[{"target":{"categories":[1]},"products":3}],"exclude":[],"value":{"type":"perc","amount":20}}',]
         ];
 
         foreach($default as $item) {
--- database/seeders/src/discounts-1.php
+++ database/seeders/src/discounts-1.php
@@ -1,18 +1,22 @@
 <?php
 $keywords = [
-    'anyCategory' => ['products in the same category', ['categories' => [0]]],
+    'anyCategory' => ['products in any categories even in the same category', ['categories' => [0]]],
     'differentCats' => ['products in different categories - not in one', ['categories' => [-1]]],
-    'categories' => 'array category IDs, products in these categories',
-    'items' => 'minimal items quantity',
-    'quantity' => 'minimal products quantity in item',
+    'categories' => ['array category IDs, products in these categories', ['categories' => [1, 5, 9,]]],
+    'products' => 'minimal products quantity in cart',
+    'quantity' => 'minimal quantity items of product',
     'exactly' => 'exactly what products have to be included or excluded - IDs; allowed to be empty',
+    'value' => [
+        'type' => ['perc' => 'percents', 'abs' => 'absolut amount', 'attention' => 'one of two'],
+        'amount' => ['amount any from percents or absolut']
+    ],
 ];
 
 $req_1 = [
     'requirements' => [
         [
             'target' => ['categories' => [-1]],
-            'items' => 2,
+            'products' => 2,
         ],
     ],
     'include' => [
@@ -31,19 +35,17 @@ $req_2 = [
     'requirements' => [
         [
             'target' => ['categories' => [3,]],
-            'items' => 1,
+            'products' => 1,
         ],
     ],
     'include' => [
         [
             'target' => ['categories' => [1,]],
-            'items' => 3,
+            'products' => 3,
         ],
     ],
     'exclude' => [
         [
-            'categories' => [4,],
-            'exactly' => [76, 82],
         ],
     ],
     'value' => ['type' => 'perc', 'amount' => 20],
