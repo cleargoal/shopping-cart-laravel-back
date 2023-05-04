60bae62e209644b9aac57d0317f7270283e049a0
best <cleargoal01@gmail.com>
2023-05-04 20:49:20 +0300

    change image retrieving

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProductResource extends JsonResource
@@ -14,24 +15,23 @@ class ProductResource extends JsonResource
      * @param  Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         if ($this->quantity === 0) {
             $invent = 'OUTOFSTOCK';
-        }
-        elseif ($this->quantity > 0 && $this->quantity < 6) {
+        } elseif ($this->quantity > 0 && $this->quantity < 6) {
             $invent = 'LOWSTOCK';
-        }
-        else {
+        } else {
             $invent = "INSTOCK";
         }
+        $imagePath = asset(Storage::url('images/product')) . '/';
         return [
             'id' => $this->id,
             'name' => $this->title,
             'code' => $this->code,
             'slug' => $this->slug,
             "description" => $this->description,
-            "image" => $this->image,
+            "image" => $imagePath . $this->image,
             'price' => $this->price,
             'category' => $this->category->title,
             'category_id' => $this->category->id,
