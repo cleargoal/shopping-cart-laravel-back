6f230b3b6017c0f53cc8f1579af662002f3296a3
best <cleargoal01@gmail.com>
2023-05-26 18:33:36 +0300

    fill 'casts' array in User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Ramsey\Uuid\UuidInterface;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'anonymous',
     ];
 
     /**
@@ -40,5 +44,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'name' => 'string',
+        'email' => 'string',
+        'password' => 'string',
+        'anonymous' => 'string',
+        'remember_token' => 'string',
     ];
 }
